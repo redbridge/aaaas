c2c57daca9c3e8bb837d54f8bd3ac13e8c137585
E. Anders Lannerback <anders@lannerback.net>
2017-04-04 17:41:31 +0200

    2.1, with secrets management :P

--- aaaas.py
+++ aaaas.py
@@ -12,10 +12,17 @@ app = Flask(__name__)
 fonts = ["banner", "big", "block", "bubble", "digital", "ivrit", "lean", "mini", "mnemonic", "script", "shadow", "slant", "small", "smscript", "smshadow", "smslant", "standard", "term",
 ]
 
+secrets = [line.strip() for line in open("/opt/aaaas/secrets")]
+
+print "Known secrets: %s" % secrets
+
 @app.route('/<secret>/<font>/<text>')
 def fancy(secret, font, text):
     if not font in fonts:
         return "only these fonts are allowed: %s" % ", ".join(fonts)
+    if not secret in secrets:
+        res = os.popen("figlet -f %s %s" % ("small", "Forbidden!")).read()
+        return Response(res, mimetype='text/plain')
     else:
         if r.get(secret):
             r[secret] = int(r[secret]) + 1
@@ -26,12 +33,12 @@ def fancy(secret, font, text):
 
 @app.route('/billing')
 def billing():
-    rows = ["%s: %d" %(x, int(r[x])) for x in r.keys()]
+    rows = ["%s: %d" %(x, int(r[x])) for x in r.keys() if x in secrets]
     res = "\n".join(rows)
     return Response(res, mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
 
 
 
