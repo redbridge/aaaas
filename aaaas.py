a24146cf2ca2ce3ea293777ca746ae382a688e5e
E. Anders Lannerback <anders@lannerback.net>
2017-04-04 12:53:40 +0200

    ascii art as a service v1.0

---
+++ aaaas.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+
+from flask import Flask, Response
+app = Flask(__name__)
+
+fonts = ["banner", "big", "block", "bubble", "digital", "ivrit", "lean", "mini", "mnemonic", "script", "shadow", "slant", "small", "smscript", "smshadow", "smslant", "standard", "term",
+]
+
+@app.route("/ascii/<text>")
+def aaaas(text):
+    res = os.popen("figlet %s" % text).read()
+    return Response(res, mimetype='text/plain')
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
